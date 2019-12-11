e92471506d75255e3045e0e6a0866d04bd5abf33
vgeroim <vgeroim.qa@gmail.com>
2019-12-11 01:53:04 +0000

    styling Header and Summary

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,14 @@ import styles from './Header.module.css'
 const Header = () => {
   return (
     <div className={styles.header}>
-      Header
+      <div className={styles.wrapper}>
+        <div className={styles.nav}>
+          <a href="#" className={styles.link}>Home</a>
+          <a href="#" className={styles.link}>Skills</a>
+          <a href="#" className={styles.link}>Projects</a>
+          <a href="#" className={styles.link}>Contacts</a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -4,7 +4,16 @@ import styles from './Summary.module.css'
 const Summary = () => {
   return (
     <div className={styles.summary}>
-      Summary
+      <div className={styles.wrapper}>
+        <div className={styles.info}>
+          <span>Hi there!</span>
+          <span>My name is Vadim Geroim</span>
+          <span>I'm a full-stack web developer</span>
+        </div>
+        <div className={styles.photo}>
+          <img src="#" alt="Profile image"/>
+        </div>
+      </div>
     </div>
   );
 }
