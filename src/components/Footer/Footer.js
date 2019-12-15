25cba6cd5c50b85175528b4566d712b274cbf16f
vgeroim <vgeroim.qa@gmail.com>
2019-12-15 21:39:55 +0000

    add styles for Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,18 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      Footer
+      <div className={styles.footerWrapper}>
+        <div className={styles.footerTitle}>Geroim Vadim</div>
+        <div className={styles.footerContent}>
+          <div className={styles.footerItem}></div>
+          <div className={styles.footerItem}></div>
+          <div className={styles.footerItem}></div>
+          <div className={styles.footerItem}></div>
+        </div>
+        <div className={styles.footerCopyright}>© 2019 all rights reserved</div>
+      </div>
     </div>
+
   );
 }
 
