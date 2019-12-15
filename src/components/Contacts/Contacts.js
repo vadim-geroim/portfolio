c4b4acadf3d6cb21c598958a58e71fce9dc1ab84
vgeroim <vgeroim.qa@gmail.com>
2019-12-15 20:04:49 +0000

    style Contacts component

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,7 +4,18 @@ import styles from './Contacts.module.css'
 const Contacts = () => {
   return (
     <div className={styles.contacts}>
-      Contacts
+      <div className={styles.contactsTitle}>Contacts</div>
+      <div className={styles.contactsForm}>
+        <form>
+          <input type="text" placeholder="Name"></input>
+          <input type="text" placeholder="E-mail"></input>
+          <textarea placeholder="Your message"></textarea>
+        </form>
+      </div>
+      <div className={styles.contactsSend}>
+        <input type="submit" value="Submit" />
+      </div>
+
     </div>
   );
 }
