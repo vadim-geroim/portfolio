ccc774592fa6d829d7cfbe0e81264e5fd1275dd6
vgeroim <vgeroim.qa@gmail.com>
2019-12-17 02:13:57 +0000

    Updates

---
+++ precache-manifest.6ca24aad3a0590036474112e1c62df2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4a7f66f601da348b2325bf0b661414",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea66f412d15426c146f9",
+    "url": "/portfolio/static/css/main.cff772e5.chunk.css"
+  },
+  {
+    "revision": "bb8d43ab825e3c67b1d7",
+    "url": "/portfolio/static/js/2.c3cc337e.chunk.js"
+  },
+  {
+    "revision": "ea66f412d15426c146f9",
+    "url": "/portfolio/static/js/main.83924db1.chunk.js"
+  },
+  {
+    "revision": "a694c17334d77ca95504",
+    "url": "/portfolio/static/js/runtime-main.eaf37d0e.js"
+  }
+]);
